471f48a5edab0e773dd7036abee9bab226f47824
Antoine1903 <antoinelecomte.al60@gmail.com>
2025-01-21 15:46:06 +0100

    Update maintab.c

--- LU3IN010/TME1/maintab.c
+++ LU3IN010/TME1/maintab.c
@@ -19,12 +19,30 @@ int main() {
     }
 
     // Initialisation et affichage du tableau sur la pile
+
+    // Afficher la mémoire avant et après l'allocation (tests effectués avec NMAX=100000)
+    PrintMem();     // Memory usage: 1452 kilobytes
     InitTab(tab, NMAX);
+    PrintMem();     // Memory usage: 1756 kilobytes
+
     PrintTab(tab, NMAX);
 
     // Initialisation et affichage du tableau sur le tas
+
+    // Afficher la mémoire avant et après l'allocation
+    PrintMem();     // Memory usage: 1756 kilobytes
     InitTab(heapTab, NMAX);
+    PrintMem();     // Memory usage: 2284 kilobytes
+
     PrintTab(heapTab, NMAX);
+
+    /* La mémoire en cours d'utilisation n'est pas maximale juste avant l'appel à
+    InitTab avec le tableau alloué sur la pile, ce qui signifie qu'elle est allouée
+    lors de la déclaration de tab. Le reste de la mémoire utilisée est ajouté pendant
+    l'appel à InitTab avec le tableau alloué dynamiquement, car effectivement, au 2ème
+    et 3ème appel de PrintMem, l'utilisation de la mémoire est la même, donc il n'y a
+    pas eu d'allocation à ce moment. */
+
 
     // Tester les fonctions SumTab et MinSumTab
     int min;
@@ -34,9 +52,7 @@ int main() {
     int minSum = MinSumTab(&min, tab, NMAX);
     printf("Sum: %d, Min: %d\n", minSum, min);
 
-    // Afficher la mémoire avant et après l'allocation
-    PrintMem();
-
     free(heapTab); // Libérer la mémoire allouée
+
     return 0;
 }
