118d15ce402901f2ec88b27b4d5d570732b8bf25
Antoine1903 <antoinelecomte.al60@gmail.com>
2025-02-01 18:27:39 +0100

    Add files via upload

--- LU3IN010/TME2/lance_commande.c
+++ LU3IN010/TME2/lance_commande.c
@@ -2,25 +2,32 @@
 #include <stdlib.h>
 
 void lance_commande(char *commande) {
-
+    
     int resultat = system(commande);
 
     if (resultat == -1) {
         // Si system() échoue
-        printf("Erreur lors de l'exécution de la commande");
+        printf("Erreur lors de l'exécution de la commande : %s\n", commande);
     } else {
-
+        // Si le processus s'est terminé normalement mais avec une erreur
         if (WIFEXITED(resultat) && WEXITSTATUS(resultat) != 0) {
-            // Si le processus s'est terminé normalement avec une erreur
-            printf("La commande a échoué avec le code de sortie : %d\n", WEXITSTATUS(resultat));
+            printf("La commande '%s' a échoué avec le code de sortie : %d\n", commande, WEXITSTATUS(resultat));
         }
     }
 }
 
-int main() {
-    // Exemple d'appel à la fonction avec une commande
-    lance_commande("ls");
-    lance_commande("nonexistent_command");  // Exemple d'échec
+int main(int argc, char *argv[]) {
+    
+    if (argc < 2) {
+        printf("Aucune commande n'a été fournie.\n");
+        return 1;
+    }
+
+    // Parcourir tous les arguments (à partir de argv[1] jusqu'à argv[argc-1])
+    for (int i = 1; i < argc; i++) {
+        printf("Exécution de la commande : %s\n", argv[i]);
+        lance_commande(argv[i]);
+    }
 
     return 0;
 }
