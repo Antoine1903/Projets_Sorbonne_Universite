2d4765acc77cd87f04a15d943d84cd72a167c44f
Antoine1903 <antoinelecomte.al60@gmail.com>
2025-02-19 15:48:45 +0100

    Add files via upload

---
+++ LU3IN017/Livre.jsx
@@ -0,0 +1,9 @@
+function Livre(props){
+    return <li>
+        <p>
+            Titre : {props.titre}
+            Auteur : {props.auteur}
+            Cote : {props.cote}
+        </p>
+    </li>
+}
